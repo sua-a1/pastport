6ccaff1db2bf5f2888ddecee4e483aabd9171fb0
sua-a1 <sude.almus@gauntletai.com>
2025-02-03 16:27:31 -0800

    Initial Commit

---
+++ pastport/pastportApp.swift
@@ -0,0 +1,17 @@
+//
+//  pastportApp.swift
+//  pastport
+//
+//  Created by Sude Almus on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct pastportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pastportTests/pastportTests.swift
@@ -0,0 +1,17 @@
+//
+//  pastportTests.swift
+//  pastportTests
+//
+//  Created by Sude Almus on 2/3/25.
+//
+
+import Testing
+@testable import pastport
+
+struct pastportTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
